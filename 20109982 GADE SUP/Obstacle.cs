44d988f6a6c79a15bd0e2725820ce5994ab68c1c
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2022-01-31 08:21:49 +0200

    Create Obstacle.cs

---
+++ 20109982 GADE SUP/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAsk1_v._2
+{
+    [Serializable]
+    class Obstacle : Tile
+    {
+        private string Symbol;
+
+        public string symbol { get; set; }
+
+        public Obstacle(int x_coordinate, int y_coordinate) : base(x_coordinate, y_coordinate)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return " * ";
+        }
+    }
+
+}
