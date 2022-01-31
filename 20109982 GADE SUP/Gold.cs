0b2aa726e48bd1a1db9b45ea1ed37401b8de9d3a
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2022-01-31 08:41:22 +0200

    Create Gold.cs

---
+++ 20109982 GADE SUP/Gold.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20109982_GADE_SUP
+{
+    [Serializable]
+    class Gold : Item
+    {
+        private Random random = new Random();
+
+
+
+        public Gold(int X_coordinate, int Y_coordinate) : base(X_coordinate, Y_coordinate)
+        {
+            this.Goldworth = random.Next(0, 6);
+
+        }
+
+        public override string ToString()
+        {
+            return " g ";
+        }
+    }
+}
