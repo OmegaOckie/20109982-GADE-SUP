a2f9ba524126a88603d82c2930dc0be968571ca1
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2022-01-31 08:21:10 +0200

    Added EmptyTile Class

--- 20109982 GADE SUP/EmptyTile.cs
+++ 20109982 GADE SUP/EmptyTile.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _20109982_GADE_SUP
+namespace TAsk1_v._2
 {
-    class EmptyTile
+    [Serializable]
+    class EmptyTile : Tile
     {
+        public EmptyTile(int x_coordinate, int y_coordinate) : base(x_coordinate, y_coordinate)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return " . ";
+        }
     }
 }
