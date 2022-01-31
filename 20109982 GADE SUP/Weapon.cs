5ebf1d6829ac4fa3a0797269dc4d725463eaade0
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2022-01-31 09:22:43 +0200

    Update Weapon.cs

--- 20109982 GADE SUP/Weapon.cs
+++ 20109982 GADE SUP/Weapon.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace _20109982_GADE_SUP
 {
-    class Weapon
+    [Serializable]
+    abstract class weapon : Item
     {
+
+        private int damage;
+        private int range;
+        private int durability;
+        private int cost;
+        private string weapon_type;
+        private Char symbol;
+
+        public weapon(int X_coordinate, int Y_coordinate, char Symbol) : base(X_coordinate, Y_coordinate)
+        {
+            symbol = Symbol;
+        }
+
+        public int Damage { get => damage; set => damage = value; }
+
+        public int Durability { get => durability; set => durability = value; }
+        public int Cost { get => cost; set => cost = value; }
+        public string Weapon_type { get => weapon_type; set => weapon_type = value; }
+        public int Range { get => range; set => range = value; }
+
     }
 }
