25588a42034328024f7c5ce265253ac7109af85b
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2022-01-31 08:40:40 +0200

    Create Item.cs

---
+++ 20109982 GADE SUP/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20109982_GADE_SUP
+{
+    [Serializable]
+    abstract class Item : Tile
+    {
+        private int goldworth;
+
+        public Item(int X_coordinate, int Y_coordinate) : base(X_coordinate, Y_coordinate)
+        {
+            X_coordinate = x_coordinate;
+            Y_coordinate = y_coordinate;
+
+
+        }
+
+        public int Goldworth { get => goldworth; set => goldworth = value; }
+
+        public abstract override string ToString();
+    }
+}
