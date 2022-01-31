01caebb253805722800cd2bcb41954df35da8d61
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2022-01-31 09:26:26 +0200

    Create Leader.cs

---
+++ 20109982 GADE SUP/Leader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20109982_GADE_SUP
+{
+    class Leader : Enemy
+    {
+        public Leader(int x_coordinate, int y_coordinate) : base(x_coordinate, y_coordinate, 2, " L", 20)
+        {
+            X_coordinate = x_coordinate;
+            Y_coordinate = y_coordinate;
+            this.HP = 20;
+            this.Damage = 2;
+            this.gold = 2;
+            Weapon = new Melee_weapon(this.x_coordinate, this.y_coordinate, 'S', Melee_weapon.Type.Longsword);
+
+        }
+
+        public override Movement Returnmove(Movement move = Movement.No_Movement)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            return "Equipped: leader at [" + X_coordinate + "," + Y_coordinate + "]" + "HP:" + this.HP + "with(" + Weapon.Durability + "DURx" + Weapon.Damage + "DMG)";
+        }
+    }
+}
