02459f1b233bd40f1993bcec16f3577cb6c43f6a
Ockert Michiel van Wyk <20109982@vegaconnect.co.za>
2022-01-31 08:20:41 +0200

    Added Tile Class

---
+++ 20109982 GADE SUP/Tile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAsk1_v._2
+{
+    [Serializable]
+    abstract class Tile
+    {
+
+        protected int x_coordinate;
+
+        protected int y_coordinate;
+
+        public int X_coordinate { get => x_coordinate; set => x_coordinate = value; }
+        public int Y_coordinate { get => y_coordinate; set => y_coordinate = value; }
+
+        public enum TileType
+        {
+            HERO, ENEMY, GOLD, WEAPON, SHOP
+        }
+
+        public Tile(int x_coordinate, int y_coordinate)
+        {
+            X_coordinate = x_coordinate;
+            Y_coordinate = y_coordinate;
+
+        }
+
+
+
+
+    }
+}
